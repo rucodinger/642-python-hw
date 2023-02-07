c52401f2a274879ed39db4e37301010c003e6d42
admin <rucodinger@gmail.com>
2023-02-07 22:06:54 +0300

    "Add 01.12 hw"

---
+++ 01.12/n3.py
@@ -0,0 +1,3 @@
+first, second, third = input(), input(), input()
+
+print(third, second, first, sep="\n")
---
+++ 01.12/n6.py
@@ -0,0 +1,6 @@
+screen = int(input("Цена монитора: "))
+system_unit = int(input("Цена системного блока: "))
+mouse = int(input("Цена мыши: "))
+keyboard = int(input("Цена клавиатуры: "))
+
+print("Стоимость трех компьютеров:", (screen + system_unit + mouse + keyboard) * 3)
